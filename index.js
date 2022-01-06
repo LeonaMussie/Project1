08a48cb1ba33cf42994729fd5062d9a7267d8594
Leona <leonamussie@gmail.com>
2022-01-06 02:06:08 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("myslides");
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if(slideIndex > slides.length)
+    {slideIndex = 1}
+       slides[slideIndex-1].style.display ="block";
+       setTimeout(showSlides, 3000);
+}
+
+const accordion = document.getElementsByClassName('container');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
+
